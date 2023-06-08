00ba3a6c94b38ed6c86e111ba48cfed755eed703
Denis Skalin <den.skalin@gmail.com>
2023-06-08 12:07:10 +0300

    Start to dev Social Network

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return <header className="header">
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png"
+    alt=""
+  />
+</header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+  return (
+    <div className="profile">
+          <div>
+            <img
+              src=" https://i.ytimg.com/vi/8tNeBtR5cvg/maxresdefault.jpg"
+              alt=""
+            />
+          </div>
+          <div>avatar + descr</div>
+          <div>
+            My Posts
+            <div>New post</div>
+            <div>
+              <div>Post 1</div>
+              <div>Post 2</div>
+              <div>Post 3</div>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default Profile
